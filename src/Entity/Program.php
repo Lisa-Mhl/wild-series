bb4f1147542ce49a1e1dbb99b84fddb775dbdcb2
lilou <michellisa.75@gmail.com>
2020-06-03 11:27:01 +0200

    13_Validation

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,10 +6,15 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @UniqueEntity(fields={"title"},
+ *     errorPath="title",
+ *     message="ce titre existe déjà")
  */
 class Program
 {
@@ -21,12 +26,19 @@ class Program
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     * @Assert\Length(max="255")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *     pattern="/(plus belle la vie)/",
+     *     match=false,
+     *     message="On parle de vraies séries ici")
      */
     private $summary;
 
